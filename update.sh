0b2250be9ec1adfc1c9a39a255ea37e4a4e171b1
Werevart <160262233+Wrevart@users.noreply.github.com>
2024-04-09 22:37:31 +0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
 UBUNTU_VERSION=$(lsb_release -rs)
-echo "Версия Ubuntu: $UBUNTU_VERSION"
-echo "Обновление списка пакетов..."
+echo -e "${YELLOW}Версия Ubuntu: $UBUNTU_VERSION${NC}"
+
+echo -e "${GREEN}Обновление списка пакетов...${NC}"
 sudo apt update
-echo "Обновление установленных пакетов..."
+echo -e "${GREEN}Обновление установленных пакетов...${NC}"
 sudo apt upgrade -y
-echo "Удаление неиспользуемых пакетов..."
+echo -e "${GREEN}Удаление неиспользуемых пакетов...${NC}"
 sudo apt autoremove -y
-echo "Очистка кэша..."
+echo -e "${GREEN}Очистка кэша...${NC}"
 sudo apt autoclean
-echo "Обновление завершено."
+echo -e "${GREEN}Обновление завершено.${NC}"
+
+UBUNTU_VERSION=$(lsb_release -rs)
+echo -e "${GREEN}Версия Ubuntu: $UBUNTU_VERSION ${NC}"
